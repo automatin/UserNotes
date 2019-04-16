bf44c39874d4fbd99710b276d1251132813f7943
Ramiro Hernandez <ramiro_lfc@hotmail.com>
2019-04-16 15:54:05 -0300

    Adding a model

---
+++ UserNotes/Models/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserNotes.Models
+{
+    public class Note
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public User User { get; set; }
+    }
+}
